6340ab9c7a8cc5942f937312983c4c511654fdd6
Jason Rai <jrai@homex.com>
2020-01-03 10:53:01 -0500

    better tests

--- test/widget/home_test.dart
+++ test/widget/home_test.dart
@@ -1,7 +1,10 @@
+import 'package:beyond/service/api_service.dart';
 import 'package:beyond/ui/home/home_page.dart';
 import 'package:beyond/ui/home/home_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../mocks.dart';
 
@@ -35,5 +38,46 @@ void main() {
     // Assert: Verify UI
     expect(find.text("42 road"), findsOneWidget);
     expect(find.text("POI is 10 meters away"), findsOneWidget);
+  });
+
+  testWidgets('Display loading shimmer when loading',
+      (WidgetTester tester) async {
+    // Arrange: Override mocks to be slower
+    when(serviceLocator.apiService.getPointsOfInterest(any, any, any))
+        .thenAnswer(
+      (_) => Future.delayed(
+        Duration(seconds: 1),
+        () => ApiResponse(
+          200,
+          data: [
+            PointOfInterest(lat: "42", lon: "42", name: "POI", distance: 10)
+          ],
+        ),
+      ),
+    );
+    when(serviceLocator.apiService.getPlace(any, any)).thenAnswer(
+      (_) => Future.delayed(
+        Duration(seconds: 1),
+        () => ApiResponse(
+          200,
+          data: Place(
+            lat: "42",
+            lon: "42",
+            displayName: "Place",
+            address: Address(city: "city", road: "road", houseNumber: "42"),
+          ),
+        ),
+      ),
+    );
+    createSystemUnderTest();
+
+    // Act: Pump page to test
+    await tester.pumpWidget(homePage);
+
+    // Assert: Verify UI
+    expect(find.byType(Shimmer), findsNWidgets(6));
+
+    // Cleanup: Timers
+    await tester.pumpAndSettle();
   });
 }
